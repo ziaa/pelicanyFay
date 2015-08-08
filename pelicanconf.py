0df86c059fddc4b0bb266ae8c66a0d42774d522d
Ziaa <s.zia.azimi@gmail.com>
2015-08-08 11:54:04 +0430

    Add Pelican-RTL-Theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
+import os
 
 AUTHOR = 'Ziaa'
 SITENAME = 'Pelican Persian blog starting point'
 SITEURL = ''
 
 PATH = 'content'
+SCRIPT_PATH = os.path.realpath(__file__)
+BLOG_BASE_PATH = os.path.dirname(SCRIPT_PATH)
 
 TIMEZONE = 'Asia/Tehran'
 
@@ -33,3 +36,7 @@ DEFAULT_PAGINATION = 5
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+# Set the theme
+THEMES_PATH = os.path.join(BLOG_BASE_PATH, 'themes/')
+THEME = os.path.join(THEMES_PATH, 'Pelican-RTL-theme/')
